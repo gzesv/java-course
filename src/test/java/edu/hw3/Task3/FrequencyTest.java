e89dd4a1952df21f28b8fdfe55a0967f0ffb373d
gzevs <grom_zetom@mail.ru>
2023-10-30 00:51:28 +0400

    Add homework 3

--- src/test/java/edu/hw3/Task3/FrequencyTest.java
+++ src/test/java/edu/hw3/Task3/FrequencyTest.java
@@ -11,7 +11,7 @@ class FrequencyTest {
     @Test
     @DisplayName("Частотный словарь строк")
     void freqDict_string_() {
-        List input = List.of("this", "and", "that", "and");
+        List<String> input = List.of("this", "and", "that", "and");
         HashMap<String, Integer> answer = new HashMap<>() {{
             put("this", 1);
             put("and", 2);
@@ -27,13 +27,13 @@ class FrequencyTest {
     @DisplayName("Частотный словарь цифр")
     void freqDict_numeric_test() {
 
-        List input = List.of(1, 1, 2, 2);
+        List<Integer> input = List.of(1, 1, 2, 2);
         HashMap<Integer, Integer> answer = new HashMap<>() {{
             put(1, 2);
             put(2, 2);
         }};
 
-        Map<String, Integer> map = Frequency.freqDict(input);
+        Map<Integer, Integer> map = Frequency.freqDict(input);
 
         assertThat(map).isNotNull().isEqualTo(answer);
     }
--- src/test/java/edu/hw3/Task7/NullComparatorTest.java
+++ src/test/java/edu/hw3/Task7/NullComparatorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.TreeMap;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class NullComparatorTest {
     @Test
