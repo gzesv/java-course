790737784e8cd846a5cd973fbd8636077b46e604
gzevs <grom_zetom@mail.ru>
2023-12-03 23:09:42 +0400

    Add homework 7

--- src/main/java/edu/hw8/Task1/Client.java
+++ src/main/java/edu/hw8/Task1/Client.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
     private static final String SERVER_IP = "localhost";
