faf0f6727465e14ea9865d02a9c92af0c1b03fc8
gzevs <grom_zetom@mail.ru>
2023-12-12 14:05:25 +0400

    hw 8

--- src/main/java/edu/hw8/Task2/Fibonacci.java
+++ src/main/java/edu/hw8/Task2/Fibonacci.java
@@ -3,9 +3,17 @@ package edu.hw8.Task2;
 @SuppressWarnings("HideUtilityClassConstructor")
 public class Fibonacci {
     public static int getFibonacci(int element) {
-        if (element == 1 || element == 0) {
+        if (element <= 1) {
             return element;
         }
-        return getFibonacci(element - 1) + getFibonacci(element - 2);
+
+        int first = 0;
+        int second = 1;
+        for (int i = 2; i <= element; i++) {
+            int summary = first + second;
+            first = second;
+            second = summary;
+        }
+        return second;
     }
 }
--- src/test/java/edu/hw8/Task1/ClientServerTest.java
+++ src/test/java/edu/hw8/Task1/ClientServerTest.java
@@ -1,26 +1,20 @@
 package edu.hw8.Task1;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.security.SecureRandom;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.Stream;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ClientServerTest {
     private static Server server;
     private static final String DEFAULT_VALUE = "Ничего не найдено(";
+    private static final List<String> words = List.of(
+        "личности",
+        "оскорбления"
+    );
+    private SecureRandom random = new SecureRandom();
 
     @Test
     void getQuotes_test() {
@@ -33,7 +27,7 @@ class ClientServerTest {
     }
 
     @Test
-    void findUnknownResponse() {
+    void findUnknownResponse_test() {
         String keyword = "1";
 
         String response = Quotes.getQuotes(keyword);
@@ -42,9 +36,8 @@ class ClientServerTest {
     }
 
     @Test
-    @DisplayName("Отправка запросов на сервер в несколько потоков.")
-    void sendWordsToServer() throws InterruptedException {
-
+    @DisplayName("Отправка запросов на сервер")
+    void sendWordsToServer_tset() throws InterruptedException {
         Thread thread = new Thread(() -> {
             server = new Server();
             server.serverRun();
@@ -52,31 +45,10 @@ class ClientServerTest {
         thread.start();
         Thread.sleep(1000);
 
-        List<String> words = List.of(
-            "личности",
-            "оскорбления"
-        );
+        String word = words.get(random.nextInt(0, words.size()));
+        Client client = new Client();
+        String answer = client.clientRun(word);
 
-        SecureRandom random = new SecureRandom();
-
-        ExecutorService executorService = Executors.newCachedThreadPool();
-
-        Callable<Void> sendWord = () -> {
-            String word = words.get(random.nextInt(0, words.size()));
-            Client client = new Client();
-            String answer = client.clientRun(word);
-            assertThat(Quotes.getQuotes(word)).contains(answer);
-            return null;
-        };
-
-        var tasks = Stream.generate(() -> sendWord).limit(3).toList();
-        try {
-            List<Future<Void>> futures = executorService.invokeAll(tasks);
-            for (var future : futures) {
-                future.get();
-            }
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
+        assertThat(Quotes.getQuotes(word)).contains(answer);
     }
 }
